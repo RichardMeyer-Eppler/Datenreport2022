15ad7b6c83eaed9f692a9ab6c17bbab0a70d66d7
Richard Meyer-Eppler <richard.meyer-eppler@uv.ruhr-uni-bochum.de>
2022-03-30 13:17:50 +0200

    Max length of file_name is now enforced, default is 75 characters

--- R/get_report_df.R
+++ R/get_report_df.R
@@ -1,19 +1,39 @@
 #' Cleans up the report data
 #'
 #' @param df Data frame obtained by load_raw_reports
+#' @param max_file_name_length Integer, maximum length of file names
 #'
 #' @return Data frame
 #' @export
 #'
 #' @examples
 get_report_df <- function(
-  df
+  df,
+  max_file_name_length = 75L
 ) {
+  max_length_sans_ext <- max_file_name_length - 5L
+
   df_report <- df %>%
     dplyr::mutate(
+      file_name_orig = file_name,
       file_name = stringi::stri_trans_general(
         file_name,
         "de-ASCII; Latin-ASCII"
+      ),
+      file_name = dplyr::if_else(
+        fs::path_ext_remove(
+          file_name
+        ) > max_length_sans_ext,
+        paste0(
+          stringr::str_sub(
+            fs::path_ext_remove(
+              file_name
+            ),
+            end = max_length_sans_ext
+          ),
+          ".docx"
+        ),
+        file_name
       ),
       output_path = here::here(
         "output",
