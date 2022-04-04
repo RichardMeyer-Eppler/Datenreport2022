620beea785f9605ae1d6f75d6b3e4027d4879463
Richard Meyer-Eppler <richard.meyer-eppler@uv.ruhr-uni-bochum.de>
2022-04-04 22:45:46 +0200

    Vectorized chunk templates, added get_figure_chunk_df and get_figure_chunk_text instead of get_chunk_parameters

--- R/figures_template.R
+++ R/figures_template.R
@@ -1,14 +1,14 @@
-#' Get parameters for creating the template chunks plotting all figures
+#' Get code chunks for plotting all figures
 #'
-#' @param df Data frame with columns figure_count
+#' @param df Data frame with columns `figure_count`
 #'
-#' @return Data frame with columns chunk_label, function_parameters and
-#'         function_call
+#' @return Data frame with columns `figure_count`, `chunk_heading`,
+#'         `chunk_subheading`, `chunk_figure_df` and `chunk_plot_figure`
 #' @export
 #'
 #' @examples
-#' get_chunk_parameters(RUBer::df_example)
-get_chunk_parameters <- function(
+#' get_figure_chunk_df(RUBer::df_example %>% dplyr::filter(report_nr == 1L))
+get_figure_chunk_df <- function(
   df,
   function_call = "RUBer::plot_figure"
 ) {
@@ -25,80 +25,87 @@ get_chunk_parameters <- function(
       .data[["heading"]],
       .data[["subheading"]],
       .data[["figure_caption"]],
-      .data[["figure_height"]],
+      .data[["figure_height"]]
     ) %>%
     dplyr::mutate(
-      chunk_label = .data[["figure_count"]],
+      chunk_label = stringr::str_pad(
+        .data[["figure_count"]],
+        width = 3L,
+        pad = "0"
+      ),
       function_parameters = glue::glue(
         "df, figure_count = {figure_count}"
       ),
-      function_call = glue::glue(
-        "{function_call}({function_parameters})"
-      ),
       .before = .data[["is_heading"]]
     ) %>%
-    dplyr::select(
-      -.data[["figure_count"]]
-    ) %>%
-    dplyr::rowwise() %>%
     dplyr::mutate(
-      chunk_figure_df = list(
-        tpl_get_figure_df(
-          chunk_label = chunk_label,
-          function_params = function_parameters
+      chunk_heading = dplyr::if_else(
+        .data[["is_heading"]],
+        tpl_heading(
+          chunk_label = .data[["chunk_label"]],
+          heading = .data[["heading"]]
+        ),
+        list(
+          NA_character_
         )
+      ),
+      chunk_subheading = dplyr::if_else(
+        .data[["is_subheading"]],
+        tpl_subheading(
+          chunk_label = .data[["chunk_label"]],
+          subheading = .data[["subheading"]]
+        ),
+        list(
+          NA_character_
+        )
+      ),
+      chunk_figure_df = tpl_get_figure_df(
+        chunk_label = .data[["chunk_label"]],
+        function_params = .data[["function_parameters"]]
+      ),
+      chunk_plot_figure = tpl_plot_figure(
+        chunk_label = .data[["chunk_label"]],
+        figure_caption = .data[["figure_caption"]],
+        figure_height = .data[["figure_height"]]
       )
-      # ,
-      # chunk_heading = dplyr::if_else(
-      #   is_heading,
-      #   Datenreport2022::tpl_heading(),
-      #   NA_character_
-      # ),
-      # chunk_subheading = dplyr::if_else(
-      #   is_subheading,
-      #   Datenreport2022::tpl_subheading(),
-      #   NA_character_
-      # ),
-      # chunk_plot_figure = Datenreport2022::tpl_plot_figure()
     ) %>%
-    dplyr::ungroup()
+    dplyr::select(
+      1L,
+      tidyselect::last_col(
+        offset = 3L
+      ) : tidyselect::last_col()
+    )
 
   return(chunk_parameters)
 }
 
-#
-# Datenreport2022::tpl_get_figure_df()
-# Datenreport2022::tpl_heading()
-# Datenreport2022::tpl_subheading()
-# Datenreport2022::tpl_plot_figure()
-#
-#
-# debugonce(tpl_get_figure_df)
-#
-# test <- get_chunk_parameters(
-#   RUBer::df_example %>%
-#     dplyr::filter(
-#       report_nr == 1L
-#     )
-#   )
-# test
-#
-# test$chunk_figure_df
-#
-# RUBer::df_example %>%
-#   dplyr::group_by(
-#     report_nr,
-#     figure_count,
-#     is_heading,
-#     is_subheading
-#   ) %>%
-#   dplyr::slice_head(
-#     n = 1L
-#   ) %>%
-#   dplyr::ungroup() %>%
-#   dplyr::select(
-#     report_nr,
-#     figure_count,
-#     is_heading,
-#     is_subheading
-#   ) %>% View()
+#' Turn data frame obtained by `get_figure_chunk_df` into character vector of
+#' code chunks
+#'
+#' @param df Data frame obtained by `get_figure_chunk_df`
+#'
+#' @return Vector with chunk texts
+#' @export
+#'
+#' @examples
+#' get_figure_chunk_text(get_figure_chunk_df(RUBer::df_example %>% dplyr::filter(report_nr == 1L)))
+get_figure_chunk_text <- function(
+  df
+) {
+  chunk_text <- df %>%
+    tidyr::pivot_longer(
+      2L : tidyselect::last_col(),
+      values_to = "chunk_text"
+    ) %>%
+    dplyr::select(
+      .data[["chunk_text"]]
+    ) %>%
+    dplyr::filter(
+      !is.na(
+        .data[["chunk_text"]]
+      )
+    ) %>%
+    unlist()
+
+  return(chunk_text)
+}
